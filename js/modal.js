93d971a35673b9ab39295be0e58f20f116181e20
PavlovaGalina <65108323+PavlovaGalina@users.noreply.github.com>
2020-10-25 12:38:09 +0300

    Интерактивные элементы

--- js/modal.js
+++ js/modal.js
@@ -1,4 +1,5 @@
 const modalButton = document.querySelector(".modal-button");
+const modalWrapper = document.querySelector(".modal-wrapper");
 const modalPopup = document.querySelector(".modal");
 const searchForm = modalPopup.querySelector(".hotel-search-form");
 const arrivalDate = modalPopup.querySelector(".arrival-date");
@@ -7,27 +8,42 @@ const adultsNumber = modalPopup.querySelector(".adults-number");
 const childrenNumber = modalPopup.querySelector(".children-number");
 
 let isStorageSupport = true;
-let storage = "";
+let storageAdults = "";
+let storageChildren = "";
 
 try {
-  storage = localStorage.getItem("login");
+  storageAdults = localStorage.getItem("adults");
+  storageChildren = localStorage.getItem("children");
 } catch (err) {
   isStorageSupport = false;
 }
 
+
 modalButton.addEventListener("click", function (evt) {
   evt.preventDefault();
-  modalPopup.classList.toggle("modal-show");
-  modalPopup.classList.toggle("modal-close");
-});
+  if (modalPopup.classList.contains("modal-close")) {
+    modalPopup.classList.remove("modal-close");
+    modalPopup.classList.add("modal-show");
 
-searchForm.addEventListener("submit", function (evt) {
-    evt.preventDefault();
+    if (storageAdults) {
+    localStorage.value = storageAdults;
+    }
+
+    if (storageChildren) {
+    localStorage.value = storageChildren;
+    }
+
+    arrivalDate.focus();
+  } else {
+    modalPopup.classList.remove("modal-show");
+    modalPopup.classList.add("modal-close");
+  }
 });
 
 searchForm.addEventListener("submit", function (evt) {
   if (!arrivalDate.value || !leavingDate.value || !adultsNumber.value || !childrenNumber.value) {
     evt.preventDefault();
+    modalWrapper.classList.add("modal-error");
   } else {
     if (isStorageSupport) {
     localStorage.setItem("adults", adultsNumber.value);
@@ -38,9 +54,9 @@ searchForm.addEventListener("submit", function (evt) {
 
 window.addEventListener("keydown", function (evt) {
   if (evt.keyCode === 27) {
-    if (loginPopup.classList.contains("modal-show")) {
+    if (modalPopup.classList.contains("modal-show")) {
       evt.preventDefault();
-      loginPopup.classList.remove("modal-show");
+      modalPopup.classList.remove("modal-show");
     }
   }
 });
